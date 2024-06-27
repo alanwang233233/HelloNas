bdfa119c3011f98ac981915e8ad3e68777f32209
Error404 <wangzihao2010@outlook.com>
2024-06-27 13:04:39 +0800

    Add Log System

--- python/appcore.py
+++ python/appcore.py
@@ -1,27 +1,71 @@
 import json
 import os
+from datetime import datetime
+import uuid
 
 
 class AppCore:
-    @staticmethod
-    def json_load_data(jsonfile):
+
+    def json_load_data(self, jsonfile):
         try:
-            with open('./data/' + jsonfile, 'w', encoding='utf-8') as f:
+            with open('./data/' + jsonfile, 'r', encoding='utf-8') as f:
                 return json.load(f)
         except FileNotFoundError:
-            os.mknod('./data/' + jsonfile)
             try:
-                with open('./data/' + jsonfile, 'w', encoding='utf-8') as f:
+                os.mknod('./data/' + jsonfile)
+                with open('./data/' + jsonfile, 'r', encoding='utf-8') as f:
                     json.dump({}, f)
-            except:
-                # self.new_log('Error',e)
+            except Exception as e:
+                self.new_log('Error', e)
                 return {}
-            # self.new_log('Warning','file not found'+jsonfile+',now created')
+            self.new_log('Warning', 'file not found' + jsonfile + ',now created')
             return {}
         except Exception as e:
-            # self.new_log('Error',e)
+            self.new_log('Error', e)
             return {}
 
+    def json_write_data(self, jsonfile, content):
+        try:
+            with open('./data/' + jsonfile, 'w', encoding='utf-8') as f:
+                json.dump(content, f, indent=4, ensure_ascii=False)
+                return True
+        except FileNotFoundError:
+            try:
+                os.mknod('./data/' + jsonfile)
+                with open('./data/' + jsonfile, 'w', encoding='utf-8') as f:
+                    json.dump({}, f)
+            except Exception as e:
+                self.new_log('Error', e)
+                return False
+            self.new_log('Warning', 'file not found' + jsonfile + ',now created')
+            return False
+        except Exception as e:
+            self.new_log('Error', e)
+            return False
+
     @staticmethod
-    def json_write_data(jsonfile):
-        pass
+    def today():
+        today = datetime.now()
+        formatted_date = today.strftime('%Y.%m.%d')
+        return formatted_date
+
+    def new_log(self, sign, msg):
+        uuid1 = 114514
+        content = self.json_load_data('log.json')
+        active = True
+        while active:
+            uuid1 = str(uuid.uuid4())
+            if content[self.today()][uuid1]:
+                active = True
+            else:
+                content[self.today()][uuid1] = {}
+                active = False
+        content[self.today()][uuid1]['sign'] = sign
+        content[self.today()][uuid1]['msg'] = msg
+        if self.json_write_data('log.json', content):
+            return uuid1
+        else:
+            return False
+
+    def get_log_info(self, logid, date):
+        return self.json_load_data('log.json')[date][logid]
